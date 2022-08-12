3290da5bef905d70bdc0e0311ba8baff794c4170
lakmal34 <lakmalkaviratne@gmail.com>
2022-08-12 16:51:35 +0530

    add initial ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,HostListener } from '@angular/core';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  items: MenuItem[];
+  mobileItems: MenuItem[];
   title = 'project-tool';
+  status: boolean = false;
+  public innerWidth: any;
+  @HostListener('window:resize', ['$event'])
+  
+  onResize(event) {
+    if(window.innerWidth < 991){
+      this.status = true;  
+    }else{
+      this.status = false;  
+    }
+  }
+
+  clickEvent(){
+      this.status = !this.status;       
+  }
+
+  ngOnInit() {
+    this.status = false;  
+    this.items = [
+      {
+        label: 'Projects',
+        icon:'pi pi-fw pi-th-large',
+        items: [
+            {
+                label: 'Overview'
+            },
+            {
+                label: 'All Projects'
+            }
+        ]
+      }
+    ]
+    this.mobileItems = [
+      {
+        icon:'pi pi-fw pi-th-large',
+        items:[
+            {
+              label:'Overview'
+            },
+            {
+              label:'All Projects'
+            },
+        ]
+      },
+    ]
+    this.innerWidth = window.innerWidth;
+    if(window.innerWidth < 991){
+      this.status = true;  
+    }else{
+      this.status = false;  
+    }
+  }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ButtonModule} from 'primeng/button';
+import {PanelMenuModule} from 'primeng/panelmenu';
+import { TieredMenuModule } from 'primeng/tieredmenu';
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    PanelMenuModule,
+    BrowserAnimationsModule,
+    TieredMenuModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
